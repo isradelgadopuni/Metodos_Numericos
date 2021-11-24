9a862e2cbaefe890c95ec0c21a5552241e69e164
CristiamR <91905921+CristiamR@users.noreply.github.com>
2021-11-24 15:57:49 -0500

    Update main_PUNTO_fijo.m

--- Metodos de solución de ecuaciones lineales/Punto Fijo/main_PUNTO_fijo.m
+++ Metodos de solución de ecuaciones lineales/Punto Fijo/main_PUNTO_fijo.m
@@ -11,9 +11,15 @@
 
 % Problema 1:
 %   
-%   Descripción del problema 1
-% 
+% La ecuacion que se detalla a continuacion nos muestra la corriente a la salida dependiendo del voltaje que se le aplique a la entrada
+% calcule que voltaje se aplica, para producir una salida de 0 en la corriente en dicho punto 
+%
 
+% Problema 2
+%
+% La proporcion de un transformador esta denotada por la siguiente ecuacion, la misma que nos da la proporcion con la que se multiplica o divide el voltaje en funcion
+% de lo requerido, utilizando esta expresion encuentre las raices en las cuales aplicando un voltaje a la entrada produce que la salida sea 0, es decir encuentre los fallos en 
+% la proporcion que no proporcionan un correcto funcionamiento del transformador.
 
 %f = @(x) (2*x.^3 - 11.7*x.^2 -5)/(-17.7);
 %f = inline(f)
@@ -35,7 +41,7 @@ datos=""
 try
   
   [xn,datos,i] = PUNTO_FIJO (f, g, xi);
-  fprintf('El valor de corte es y=%d, el número de iteraciones es %d\n',xn,i);
+  fprintf('El valor de corte es y=%d, el nÃºmero de iteraciones es %d\n',xn,i);
   hold on
   ezplot(g)
   grid on
