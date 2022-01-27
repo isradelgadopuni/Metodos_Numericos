82c3c8602b40cfc805084acb2b7f5c295d49e7bd
sjcdjf0f909990 <91700873+sjcdjf0f909990@users.noreply.github.com>
2022-01-26 23:21:01 -0500

    Add files via upload

---
+++ Ajustes de curvas e interpolación/Regresión polinomial/Regresion_polinomialll.m
@@ -0,0 +1,74 @@
+%REGRESIÓN POLINOMIAL    Ajuste de curvas.
+%   REGRESION POLINOMIAL(datos,grado,x,y,M,b) 
+%       [y_x ,error,vect,p]=Regresion_polinomial(n,m,x,y,M,b)
+%       y_x = función apromixada
+%       error = error de la curva
+%       vect = vector con todos los valores desde el punto 
+%       inicial de las abscisas hasta el punto final
+%       p = vector con todos los valores evaluados en la funcion obtenida
+%       n = numero de datos a ingresar (longitud del vector con los valores iniciales)
+%       m = grado del polinomio
+%       x = vector con los valores dados de las abscisas
+%       y = vector con los valores dados de las ordenadas
+%       M = matriz de ceros
+%       b = matriz de ceros
+
+
+function [y_t,y_x,vect,c]=Regresion_polinomialll(n,m,x,y,M,b)
+  
+%Se generan la matriz y el vector
+for i=1:m+1
+  for j=1:m+1
+    for k=1:n
+      M(i,j)=M(i,j)+x(k)^(i+j-2);
+    endfor
+  endfor
+endfor
+
+for i=1:m+1
+  for k=1:n
+    b(i)=b(i)+(x(k)^(i-1))*y(k);
+  endfor
+endfor
+%Se calcula el vector solucion
+a=inv(M)*b
+%Se coloca el vector de coeficientes en orden decreciente de la potencia
+c=fliplr(a');
+
+%Se ewcribe el polinomio de regresion
+%-------------------------------
+y_x=  poly2str(c,"t")
+y_t=y_x
+%--------
+y_x=@(t) y_x
+%-----------
+ %%%%%%%%%%%%%%%%%%%
+
+%Se asignan valores al vector t
+%------------------------------------
+t=x(1):0.01:x(n);
+vect=t;
+%-------------------
+
+%Se evalua el polinomio
+%-------------------------p=polyval(c,t);
+
+%Para calcular el error cuadratico medio 
+%t=x;
+%z=polyval(c,t);
+
+%for k=1:n
+ % e(k)=(y(k)-z(k))^2;
+%endfor
+%et=0;
+%for k=1:n
+ % et=et+e(k);
+%endfor
+
+%ecm=sqrt(et)/n
+%---------------
+%ecm
+%error=ecm
+%------------------
+
+endfunction
---
+++ Ajustes de curvas e interpolación/Regresión polinomial/main_Regresion_polinomial.m
@@ -0,0 +1,51 @@
+%n=input("Numero de datos=");
+%m=input("Grado del polinomio=");
+
+
+%Problema 1 
+%Un investigador informa sobre los datos tabulados a continuacion
+%de un experimento para determinar la tasa de crecimiento de bacterias
+%k (por día), como funcion de la concentración de oxígeno c (mg/L)
+%Se sabe que dicho datos pueden modelarse por medio de la siguiente ecuación:
+%       K=(k_max*c^2)/(c_s+c^2)
+
+% c | 0.2  0.5  0.8  1.2  1.7    2    2.3
+% -----------------------------------------
+% K | 500 700 1000  1200  2200  2650  3750
+%Utilice los datos para ajustar una parábola 
+n=7;
+m=2;
+x=[0.2 0.5 0.8 1.2 1.7 2 2.3];
+y=[500 700 1000 1200 2200 2650 3750];
+M=zeros(m+1,m+1);
+b=zeros(m+1,1);
+f=@(t) 97731/145*t.^2 - 11932/51*t + 19331/32;
+
+
+%Problema 2 
+%Ajuste  una ecuación cubica a los datos siguientes
+% x   3    4   5    7    8   9    11  12
+% ------------------------------------------
+% y   1.6 3.6 4.44 3.4 2.2  2.8  3.8  4.6 
+%n=8;
+%m=3;
+
+%x=[3 4 5 7 8 9 11 12]
+%y=[1.6 3.6 4.4 3.4 2.2 2.8 3.8 4.6]
+%M=zeros(m+1,m+1);
+%b=zeros(m+1,1);
+%f=@(t) 832/17825*t.^3 - 1415/1359*t.^2 + 3179/445*t - 5595/487;
+try 
+  [y_t,y_x,vect,c]=Regresion_polinomialll(n,m,x,y,M,b);
+  
+  plot(x,y,"o");
+  hold on
+  plot(f,[x(1),x(length(x))]);
+  grid on
+  y_t
+  y_x
+  
+%catch
+%end_try_catch
+
+end
